739e1c372052475cd6843cfc6c6314978d8c6c3a
chutium <teng.qiu@gmail.com>
2019-01-17 23:40:52 +0000

    Sandbox for transaction generation and wallet service endpoint info

--- sandbox/index.js
+++ sandbox/index.js
@@ -10,6 +10,16 @@ const address = Elastos.getAddress(publicKey.toString('hex'));
 
 const tx = new Transaction();
 
+/*
+ * Endpoint of wallet service for testnet
+ * Documentation refer to: https://walletservice.readthedocs.io/
+ */
+const testnet_endpoint = 'http://18.179.207.38:8080';
+
+const testnet_address_from = 'EJonBz8U1gYnANjSafRF9EAJW9KTwRKd6x';
+const testnet_address_to = 'EbunxcqXie6UExs5SXDbFZxr788iGGvAs9';
+const testnet_privateKey = '492f67d441f563aa4746497eb77c89906a3d3c06b242030ba966bc5604482ef7';
+
 function getAddress() {
   console.log(mnem);
   console.log(privateKey);
@@ -20,15 +30,11 @@ function getAddress() {
 }
 
 function generateRawTransactionOnTestnet() {
-  var testnet_endpoint = 'http://18.179.207.38:8080'; // Endpoint of wallet service for testnet
-                                                      // Refer to: https://walletservice.readthedocs.io/
-  var testnet_address_from = 'EJonBz8U1gYnANjSafRF9EAJW9KTwRKd6x';
-  var testnet_address_to = 'EbunxcqXie6UExs5SXDbFZxr788iGGvAs9';
-  var testnet_privateKey = '492f67d441f563aa4746497eb77c89906a3d3c06b242030ba966bc5604482ef7';
   tx.createTx('http://localhost:8081', testnet_address_from, testnet_address_to, 1000);
   console.log(tx);
   return tx.generateRawTransaction(testnet_privateKey);
-  // After you can use tx.sendRawTx(testnet_endpoint) to send the rawTx to wallet service endpoint and it returns a transaction id hash
+  // After generateRawTransaction you can use tx.sendRawTx(testnet_endpoint) to
+  // send the rawTx to wallet service endpoint and it returns a transaction id hash
 }
 
 class App extends React.Component {
@@ -37,25 +43,28 @@ class App extends React.Component {
       <div className="App">
         <div>Your ELA wallet is created, wallet address is:</div>
         <div><pre>{getAddress()}</pre></div>
-        <hr/>
+        <br /><hr /><br />
         <div>Sample code to generate a raw transaction hex string returns:</div>
         <div><pre>{generateRawTransactionOnTestnet()}</pre></div>
-        <hr />
+        <br /><hr /><br />
         <div>You can use this rawTx string to create a transaction with a REST API call:</div>
-        <div><pre>curl -XPOST -H "Content-Type: application/json" -d '{'{'}"data": "{tx.rawTx}"}' 18.179.207.38:8080/api/1/sendRawTx</pre></div>
+        <div><pre>curl -XPOST -H "Content-Type: application/json" -d '{'{'}"data": "{tx.rawTx}"}' {testnet_endpoint}/api/1/sendRawTx</pre></div>
         <div>The result of response will be the transaction id hash, like:</div>
         <div><pre>{'{'}
           "result": "9211614c5d64e7929e18929aafdd3a406564a1ebc6c79f656b5a091e5a96af72",
           "status": 200
         }</pre></div>
-        <hr />
+        <br /><hr /><br />
         <div>After the transaction getting confirmed, you can check the result with REST API call:</div>
-        <div>curl <a href="http://18.179.207.38:8080/api/1/tx/9211614c5d64e7929e18929aafdd3a406564a1ebc6c79f656b5a091e5a96af72" target="_blank">18.179.207.38:8080/api/1/tx/9211614c5d64e7929e18929aafdd3a406564a1ebc6c79f656b5a091e5a96af72</a></div>
+        <div>curl <a href={testnet_endpoint + '/api/1/tx/9211614c5d64e7929e18929aafdd3a406564a1ebc6c79f656b5a091e5a96af72'} target="blank">{testnet_endpoint}/api/1/tx/9211614c5d64e7929e18929aafdd3a406564a1ebc6c79f656b5a091e5a96af72</a></div>
+        <br />
         <div>And you can check the balance changes with:</div>
-        <div>sender: EJonBz8U1gYnANjSafRF9EAJW9KTwRKd6x</div>
-        <div>curl <a href="http://18.179.207.38:8080/api/1/balance/EJonBz8U1gYnANjSafRF9EAJW9KTwRKd6x" target="_blank">18.179.207.38:8080/api/1/balance/EJonBz8U1gYnANjSafRF9EAJW9KTwRKd6x</a></div>
-        <div>receiver: EbunxcqXie6UExs5SXDbFZxr788iGGvAs9</div>
-        <div>curl <a href="http://18.179.207.38:8080/api/1/balance/EbunxcqXie6UExs5SXDbFZxr788iGGvAs9" target="_blank">18.179.207.38:8080/api/1/balance/EbunxcqXie6UExs5SXDbFZxr788iGGvAs9</a></div>
+        <br />
+        <div>curl <a href={testnet_endpoint + '/api/1/balance/' + testnet_address_from} target="_blank">{testnet_endpoint}/api/1/balance/{testnet_address_from}</a></div>
+        <div>to check sender: {testnet_address_from}</div>
+        <br />
+        <div>curl <a href={testnet_endpoint + '/api/1/balance/' + testnet_address_to} target="_blank">{testnet_endpoint}/api/1/balance/{testnet_address_to}</a></div>
+        <div>to check receiver: {testnet_address_to}</div>
       </div>
     );
   }
